dff6c3021851b58d5a4240da5cee4e26e305d09a
George Langroudi <George.Langroudi@outlook.com>
2020-01-19 21:27:25 +0000

    First Commit

---
+++ NezTest/Game1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace NezTest.Desktop
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Core
+    {
+
+        public Game1()
+        {
+            this.Window.AllowUserResizing = true;
+            // Event obj = new Event();
+            
+        }
+    }
+}
